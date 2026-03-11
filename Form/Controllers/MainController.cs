42bec4ee3d9d20adb18cf90c4e2eb2d8e3f41add
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2026-03-11 11:42:07 +0200

    Created MVC project and changed defaul HomeController to MainController, replaced paths for nav buttons

---
+++ Form/Controllers/MainController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Form.Controllers;
+
+public class MainController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+}
