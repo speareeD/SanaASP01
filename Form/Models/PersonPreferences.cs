5cd7f60aa31bd413772cc310e69df65d701c529b
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2026-03-12 21:51:34 +0200

    Changed model stucture: added select, checkbox, radio buttons

--- Form/Models/PersonPreferences.cs
+++ Form/Models/PersonPreferences.cs
@@ -1,8 +1,23 @@
-﻿namespace Form.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Form.Models;
 
 public class PersonPreferences
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    // public string Name { get; set; }
+    [Required(ErrorMessage = "Please enter your name.")]
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; }
+    [Required(ErrorMessage = "Please enter your last name.")]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; }
+    [Required(ErrorMessage = "Please choose country.")]
+    [Display(Name = "Country")]
+    public string SelectedCountry { get; set; }
+    public List<string> Countries { get; set; } = ["USA", "Canada", "UK"];
+    
+    [Display(Name = "Subscribe to newsletter agreement")]
+    public bool SubscribeToNewsletter { get; set; }
+    [Required(ErrorMessage = "Please choose your gender.")]
+    public string Gender { get; set; }
 }
